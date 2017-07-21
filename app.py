66185bd1f7a94cafde5f3de74f13d46cd5cca855
borenho <borecollins@gmail.com>
2017-07-21 14:09:34 +0300

    Add method docstrings

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 import requests
 
 def get_url(city):
-    # You need to obtain your api key from open weather, then give my_api_key the value of your key
+    """  
+    Gets the full url of the place you want to its weather
+    You need to obtain your api key from open weather, then give my_api_key the value of your key below
+    """
+    
     my_api_key = 'fda7542e1133fa0b1b312db624464cf5'
     unit = 'metric'    # To get temperature in Celsius
     weather_query = 'http://api.openweathermap.org/data/2.5/weather?q='
     full_query = weather_query + city + '&units=' + unit + '&APPID=' + my_api
-    # This full_api_url results in smth like
+    # This full_query results in smth like
     # 'http://api.openweathermap.org/data/2.5/weather?q=Nairobi&units=metric&APPID=YOUR-KEY-HERE'
 
     return full_query
 
 def fetch_data(full_query):
+    """ 
+    Fetches data from the given url
+    """
+
     url = requests.get(full_query)
     # Parse the json dat so it can be used as a normal dict
     raw_data = url.json()
