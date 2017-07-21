d3c6123a66fa41779f9c95052193909fede76ded
borenho <borecollins@gmail.com>
2017-07-21 13:54:55 +0300

    Get city name from the user

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import requests
+
 def get_url(city):
     # You need to obtain your api key from open weather, then give my_api_key the value of your key
     my_api_key = 'fda7542e1133fa0b1b312db624464cf5'
@@ -8,3 +10,9 @@ def get_url(city):
     # 'http://api.openweathermap.org/data/2.5/weather?q=Nairobi&units=metric&APPID=YOUR-KEY-HERE'
 
     return full_query
+
+if __name__ == 'main':
+    print("-------------- Know Your City's Weather -------------")
+    print('Helooooooooooo')
+    # Get city name from the user
+    city = input("Enter your city's name: ")
