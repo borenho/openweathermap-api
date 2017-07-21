b194d71a154fc4967cc3355601b3dc85d4258188
borenho <borecollins@gmail.com>
2017-07-21 13:40:46 +0300

    Define a method that returns the full query of the opw api

---
+++ app.py
@@ -0,0 +1,10 @@
+def get_url(city):
+    # You need to obtain your api key from open weather, then give my_api_key the value of your key
+    my_api_key = 'fda7542e1133fa0b1b312db624464cf5'
+    unit = 'metric'    # To get temperature in Celsius
+    weather_query = 'http://api.openweathermap.org/data/2.5/weather?q='
+    full_query = weather_query + city + '&units=' + unit + '&APPID=' + my_api
+    # This full_api_url results in smth like
+    # 'http://api.openweathermap.org/data/2.5/weather?q=Nairobi&units=metric&APPID=YOUR-KEY-HERE'
+
+    return full_query
